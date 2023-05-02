0d171861112cddd192e14b577c9c4b4128f428e7
David Vazquez <dave_u@outlook.com>
2023-05-01 20:41:04 -0600

    se intento arreglar un bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  build:{
+  plugins: [react()],
+  build: {
     outDir: 'docs',
   },
-  plugins: [react()],
-})
+  server: {
+    historyApiFallback: true,
+  },
+});
